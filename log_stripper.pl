e708683ffd0f73c10e840caca43dcb1a525946c6
Motes, C.J <motescj@aim.com>
2016-08-12 16:46:05 -0500

    get opts long working

--- log_stripper.pl
+++ log_stripper.pl
@@ -5,10 +5,18 @@ my $LOG_FILE="log.file";
 my %names;
 my %dates;
 my %events;
+my $username;
+my $date;
+my $event;
+GetOptions (	"u" => \$username,
+		"d" => \$date,
+		"e" => \$event,
+);
 &get_fields;
             
 print "Found usernames: $_ \n" foreach (keys%names);
 print "Found dates $_ \n" foreach (keys%dates);
+#print "Found events $_ \n" foreach (keys%events) if ($event);
 print "Found events $_ \n" foreach (keys%events);
 sub get_fields {
 open HANDLE, "<", "$LOG_FILE" 
